e967e4207a08dc125b8f202428c78bc26c2e0085
Anurag Singh <anuragsingh.in@gmail.com>
2023-01-08 17:48:27 +0800

    Sample code to run cluster

---
+++ examples/eks-cluster.tf
@@ -0,0 +1,9 @@
+module "public_eks_cluster" {
+  source         = "../../"
+  vpc_name       = "my-vpc"
+  cluster_name   = "simple-eks-cluster"
+  desired_size   = 2
+  max_size       = 2
+  min_size       = 1
+  instance_types = ["t3.medium"]
+}
