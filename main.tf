9feca5d169af954f9f11544eff0890f628731ac9
Anurag Singh <anuragsingh.in@gmail.com>
2023-01-09 14:40:08 +0800

    Changed cluster role arn

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 
 resource "aws_eks_cluster" "this" {
   name       = var.cluster_name
-  role_arn   = aws_iam_role.cluster.arn
+  role_arn   = aws_iam_role.cluster_role.arn
   subnet_ids = var.subnet_ids
 
   depends_on = [
