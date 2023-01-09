5782af5bc51eda4084f91c51d0dd8387953d9ac3
Anurag Singh <anuragsingh.in@gmail.com>
2023-01-09 14:44:34 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,11 @@
 resource "aws_eks_cluster" "this" {
   name       = var.cluster_name
   role_arn   = aws_iam_role.cluster_role.arn
-  subnet_ids = var.subnet_ids
 
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+    
   depends_on = [
     aws_iam_role_policy_attachment.cluster_role_policy_attachment
   ]
